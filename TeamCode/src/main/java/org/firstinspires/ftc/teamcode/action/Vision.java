80f3051b9fafb17ff2b8cf5b6c814ad8eec30e5c
DN8417 <dgnolan09@gmail.com>
2025-03-07 16:01:05 -0500

    April Tag Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/action/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/action/Vision.java
@@ -11,13 +11,20 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import org.firstinspires.ftc.vision.apriltag.*;
 import org.firstinspires.ftc.vision.opencv.ImageRegion;
 import org.firstinspires.ftc.vision.opencv.PredominantColorProcessor;
+
+import java.util.List;
 
 public class Vision {
 
     Telemetry telemetry;
     PredominantColorProcessor colorSensor;
+    AprilTagProcessor aprilTag;
     VisionPortal portal;
     public void init(@NonNull OpMode opMode) {
 
@@ -34,8 +41,14 @@ public class Vision {
                         PredominantColorProcessor.Swatch.WHITE)
                 .build();
 
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawTagOutline(true)
+                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .build();
+
         portal = new VisionPortal.Builder()
-                .addProcessor(colorSensor)
+                .addProcessors(colorSensor, aprilTag)
                 .setCameraResolution(new Size(320, 240))
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .build();
@@ -93,9 +106,41 @@ public class Vision {
         return new double[] {hue, saturation, value};
     }
 
+    public AprilTagDetection aprilTagSearch(int id) {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                if (id == detection.metadata.id) {
+                    return detection;
+                }
+                /*
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                */
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        /*
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+        */
+        return null;
+    }
+
     public void telemetryOutput() {
         telemetry.addData("closestSwatch", closestSwatch());
         telemetry.addData("RGB", String.format("R %4d, G %4d, B %4d", getColor("red"), getColor("green"), getColor("blue")));
         telemetry.addData("HSV", String.format("H %4.0f, S %4.1f, V %4.1f", getHSVValue()[0], getHSVValue()[1], getHSVValue()[2]));
+        AprilTagDetection detection = aprilTagSearch(15);
+        if (detection != null) {
+            telemetry.addData("aprilTagName", String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+            telemetry.addData("aprilTagCoords", String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+        }
     }
 }
