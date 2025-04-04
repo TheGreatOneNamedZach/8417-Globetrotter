fd49cbf5f996b086012deeafb43f09b19c31e22f
TheGreatOneNamedZach <thegreatonenamedzach@gmail.com>
2025-04-03 21:53:12 -0400

    Added time accumulation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/action/SubmersibleServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/action/SubmersibleServo.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -20,7 +21,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  * @see Grabber
  */
 public class SubmersibleServo {
-    
+
     Telemetry telemetry; // The telemetry class object
     CRServo axisMovement; // The servo used for axis movement
     TouchSensor posTouch; // The touch sensor that moves the servo forwards
@@ -28,6 +29,10 @@ public class SubmersibleServo {
     TouchSensor limitTouch; // The touch sensor at the limit of the servo's movement boundary
     String displayName; // The name of the axis to display on telemetry
     double axisSpeedMult; // The axis-specific speed multiplier
+    ElapsedTime timer; // The axis timer for counting accumulated time
+    double timeFromHome; // The time traveled from home (the limit sensor)
+    boolean wasMoving; // Tracks if the servo was moving in the previous loop iteration
+    boolean wasMovingPositive; // Tracks the direction the servo was last moving
 
     /** <b>This is the constructor for {@link SubmersibleServo}.</b>
      * <p>
@@ -54,6 +59,13 @@ public class SubmersibleServo {
         this.limitTouch = LimitTouch;
         this.displayName = displayName;
         this.axisSpeedMult = axisSpeedMult;
+
+        this.timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS); // Creates a new timer to count elapsed time traveled from home (limit sensor)
+
+        // Sets default values
+        this.timeFromHome = 0;
+        this.wasMoving = false;
+        this.wasMovingPositive = true;
     }
 
     /** <b>Controls the servo movement on the axis.</b>
@@ -68,22 +80,45 @@ public class SubmersibleServo {
         if ((this.limitTouch.isPressed() && !this.posTouch.isPressed()) || (this.posTouch.isPressed() && this.negTouch.isPressed())) {
             this.axisMovement.setPower(0); // Set the power to 0%
 
+            if (this.limitTouch.isPressed()) {
+                this.timeFromHome = 0.0;
+            } else if (wasMoving) {
+                this.timeFromHome += this.wasMovingPositive ? this.timer.time() : -1 * this.timer.time();
+            }
+
         } else if (this.posTouch.isPressed()) {
             // ...else IF the positive touch sensor is pressed...
 
             this.axisMovement.setPower(1 * this.axisSpeedMult); // Set the power to 100%
 
+            if (!this.wasMoving) {
+                this.timer.reset();
+            } else {
+                this.wasMovingPositive = true;
+            }
         } else if (this.negTouch.isPressed()) {
             // ...else IF the negative touch sensor is pressed...
 
             this.axisMovement.setPower(-1 * this.axisSpeedMult); // Set the power to -100%
 
+            if (!this.wasMoving) {
+                this.timer.reset();
+            } else {
+                this.wasMovingPositive = false;
+            }
         } else {
             // ...else no buttons are being pressed.
 
             this.axisMovement.setPower(0); // Set the power to 0%
+
+            if (this.wasMoving) {
+                this.timeFromHome += this.wasMovingPositive ? this.timer.time() : -1 * this.timer.time();
+            }
         }
 
+        // Updates if servo was moving last iteration
+        // False IF AND ONLY IF the servo is NOT moving
+        this.wasMoving = this.axisMovement.getPower() != 0.0;
     }
 
     /** <b>Outputs relevant information about this axis to the driver screen.</b> */
@@ -93,7 +128,11 @@ public class SubmersibleServo {
                 "Servo Speed: %.2f\n" +
                 "Move Forwards: %b\n" +
                 "Move Backwards: %b\n" +
-                "Limit Sensor Touched: %b",
-                this.axisMovement.getPower(), this.posTouch.isPressed(), this.negTouch.isPressed(), this.limitTouch.isPressed());
+                "Limit Sensor Touched: %b\n" +
+                "Time From Home: %.2f\n" +
+                "Timer: %.2f\n" +
+                "Was Moving: %b\n" +
+                "Was Moving Positive: %b",
+                this.axisMovement.getPower(), this.posTouch.isPressed(), this.negTouch.isPressed(), this.limitTouch.isPressed(), this.timeFromHome, this.timer.time(), this.wasMoving, this.wasMovingPositive);
     }
 }
